2b7401726e7f70dda1154fd3ec95f6226fdc4304
Muhammad Irtaza Ghaffar <93589223+M-IrtazaGhaffar@users.noreply.github.com>
2023-05-06 22:17:43 +0500

    Update user.controllers.js

--- Controllers/user.controllers.js
+++ Controllers/user.controllers.js
@@ -170,7 +170,8 @@ const login = async (req, res) => {
     const { email, password } = req.body;
     const findUser = await userModel.find({
       email: email,
-    });
+    }, { email: 0 });
+    console.log(findUser)
     if (findUser.length !== 0) {
       const result = await bcrypt
         .compare(password, findUser[0].password)
