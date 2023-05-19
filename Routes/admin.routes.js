89de389d79371a1b16ff0e910f4a11fd5247f7d3
Muhammad Irtaza Ghaffar <93589223+M-IrtazaGhaffar@users.noreply.github.com>
2023-05-19 17:02:07 +0500

    Update admin.routes.js

--- Routes/admin.routes.js
+++ Routes/admin.routes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const { login } = require('../Controllers/admin.controllers')
 const routerAdmin = express.Router()
 
-routerAdmin.get('/', login)
+routerAdmin.get('/login', login)
 
 module.exports = {
     routerAdmin
